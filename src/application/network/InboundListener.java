b35531d9f199b391c4512a213982fe8a8f3e382b
xNicklaj <simonearonica@gmail.com>
2019-02-20 22:12:49 +0100

    Server is now closed and reopen if there's already a bind server

--- src/application/network/InboundListener.java
+++ src/application/network/InboundListener.java
@@ -14,26 +14,36 @@ public class InboundListener extends SocketInfo implements Runnable{
 	private Socket inboundSocket;
 	private String line;
 	public static MainController controller;
-	
+	private static boolean run;
+
 	public void startServer() throws IOException{
-		server = new ServerSocket(this.getPort());
+		if(server != null)
+		{
+			run = false;
+			server.close();
+			inboundSocket.close();
+			server = null;
+			inboundSocket = null;
+			server = new ServerSocket(this.getPort());
+		}
+		else
+		{
+			server = new ServerSocket(this.getPort());
+		}
 		inboundSocket = server.accept();
-		
+
 		Scanner in = new Scanner(inboundSocket.getInputStream());
-		
-		while(true) {
+		run = true;
+		while(run) {
 			try {
 				line = in.nextLine();
 				controller.addMessage(line.substring(0, line.indexOf("/")), line.substring(line.indexOf("/"), line.lastIndexOf("/")));
 			} catch (NoSuchElementException e) {
-				inboundSocket.close();
-				inboundSocket = server.accept();
-				in.close();
-				in = new Scanner(inboundSocket.getInputStream());
+				
 			}
 		}
 	}
-	
+
 	public void run()
 	{
 		try {
